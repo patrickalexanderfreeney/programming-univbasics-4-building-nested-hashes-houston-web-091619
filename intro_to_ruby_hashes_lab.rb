0cf5af7c5322c6019582d808d6b6b572f5547c3c
Patrick Alexander <patrick_alexander@mail.com>
2019-09-03 19:38:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,8 +21,8 @@ def monopoly_with_third_tier
       :four_pieces_owned=>200
     },
     :names=>{
-      :reading_railroad=>{},
-      :pennsylvania_railroad=>{},
+      :reading_railroad=>{:mortgage_value=>"$100"},
+      :pennsylvania_railroad=>{:mortgage_value=>},
       :b_and_o_railroad=>{},
       :shortline_railroad=>{}
       
